e0d7d00f3e769e2366f2c3edbd5e7af892725ca1
lakshaybxt <bishtlakshay14@gmail.com>
2025-08-05 23:53:49 +0530

    Intialized Task Management Codebase

---
+++ src/main/java/com/railse/hiring/workforcemgmt/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.railse.hiring.workforcemgmt.common.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
+
+
--- src/test/java/com/railse/hiring/ManagementApplicationTests.java
+++ src/test/java/com/railse/hiring/ManagementApplicationTests.java
@@ -1,4 +1,4 @@
-package com.workforce.management;
+package com.railse.hiring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
